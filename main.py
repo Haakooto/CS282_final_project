f77d1a827756dac94b9892bed5af3782c32501cd
haakooto <hakonolav.torvik@gmail.com>
2022-03-28 20:29:53 -0700

    start on framework for future code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import numpy as np
+import torch
+from models.bayesian.Bayes3fc import Bayesian3FC
+
+BNN = Bayesian3FC(20, 10, 10, 10)
+
+print(list(BNN.modules()))
+
+x = torch.randn(20)
+print(x)
+y = BNN(x)
+print(y)
+
