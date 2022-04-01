bba4d967d08d3785d84372abcad874fa717ac0ca
haakooto <hakonolav.torvik@gmail.com>
2022-04-01 13:08:11 -0700

    simplify layer implementation, generelise distribution

--- layers/BayesLinear.py
+++ layers/BayesLinear.py
@@ -1,4 +1,5 @@
 from .template import BaseModule
+from ..priors import implemented_dists as Dists
 from torch import nn
 from torch.nn import functional as F
 import torch
@@ -36,75 +37,53 @@ class Linear(BaseModule):
     def __init__(self, in_nodes, out_nodes, priors=None, use_bias=True):
         super().__init__()
 
-        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.device = torch.device(
+            "cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.use_bias = use_bias
+        self.frozen = False
 
-        if priors is None: priors = {'dist': 'normal'}
-        # priors.setdefault("prior_mean", 0)
-        # priors.setdefault("prior_var", 1)
-        # priors.setdefault("posterior_init")
-        # self.priors = priors
+        if priors is None:
+            priors = {"dist": "gaussian", "params": {"mu": 0, "std": 1}}
+        self.priors = priors
 
-        self.W_mu = nn.Parameter(torch.empty(out_nodes, in_nodes, device=self.device))
-        self.W_sig = nn.Parameter(torch.empty(out_nodes, in_nodes, device=self.device))
+        self.distribution = nn.Parameter(Dists[self.priors["dist"]](**self.priors["params"]))
 
-        self.b_mu = nn.Parameter(torch.empty(out_nodes, device=self.device))
-        self.b_sig = nn.Parameter(torch.empty(out_nodes, device=self.device))
-
-        self.init_params()
-
-    def init_params(self):
-        """"
-        initial sampling of parameters
-        """
-        self.W_mu = self.sample(self.W_mu.size())
-        # self.W_mu.data.normal()
-        pass
+        self.weight = nn.Parameter(torch.empty(out_nodes, in_nodes, device=self.device))
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.empty(out_nodes, device=self.device))
+        else:
+            self.register_parameter("bias", None)
+        self.sample()
 
     def sample(self, shape):
         """
         Intended to make it simple to resample parameters when training
         from a preset distribution.
         """
-        pass
+        self.weight.data = self.distribution(self.W.size())
+        if self.use_bias:
+            self.bias.data = self.distribution(self.b.size())
 
     def forward(self, x):
         """
         Pass an input x through layer.
         If layer is 'frozen', compute affine transformation
         else, resample it
-
         """
-        if self.W_eps is None:
-            W_ = self.sample(self.W_mu.size())
-        else:
-            pass
-            # W_ = self.W_eps * self.W_sig
-        W = self.W_mu + W_
+        if not self.frozen:
+            self.sample()
 
-        if self.use_bias:
-            if self.b_eps is None:
-                b_ = self.sample(self.b_mu.size())
-            else:
-                pass
-                # b_ = self.b_eps * self.b_sig
-            b = self.b_mu + b_
-        else:
-            b = None
-
-        return F.linear(x, W, b)
+        return F.linear(x, self.weight, self.bias)
 
     def freeze(self):
         """
         Fix the epsilon_parameters, such that the model always returns the same output for a given input
         """
-        self.W_sig = self.sample(self.W_mu.size())
-        if self.use_bias:
-            self.b_eps = self.sample(self.b_mu.size())
+        self.sample()
+        self.frozen = True
 
     def unfreeze(self):
         """
         Unfreezes the epsilon_parameters, such that the model resamples every time.
         """
-        self.W_eps = None
-        if self.use_bias:
-            self.b_eps = None
+        self.frozen = False
---
+++ priors/__init__.py
@@ -0,0 +1,3 @@
+from gaussian import Gaussian
+
+implemented_dists = ["gaussian": Gaussian,]
