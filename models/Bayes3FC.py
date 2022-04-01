d387b2b686db9515195bd540825188acc2cc9ee6
haakooto <hakonolav.torvik@gmail.com>
2022-04-01 16:54:00 -0700

    fix bugs in model creation, dtypes and devices

--- models/Bayes3FC.py
+++ models/Bayes3FC.py
@@ -7,14 +7,14 @@ class Bayesian3FC(BaseModule):
     Simple implementation of a 3-layered FC Bayesian net with ReLU as non-linearity
     Mainly for easy implementation, more complex models will be made later
     """
-    def __init__(self, features=2, n1=2, n2=2, n3=2, classes=1):
+    def __init__(self, features=2, n1=2, n2=2, classes=1, prior=None):
         super().__init__()
 
-        self.fc1       = BLinear(features, n1)
-        self.non_lin1  = nn.PReLU()
-        self.fc2       = BLinear(n2, n3)
+        self.fc1       = BLinear(features, n1 prior=prior)
+        self.non_lin1  = nn.ReLU()
+        self.fc2       = BLinear(n1, n2 prior=prior)
         self.non_lin2  = nn.ReLU()
-        self.fc3       = BLinear(n3, classes)
+        self.fc3       = BLinear(n2, classes prior=prior)
 
 
 
--- priors/gaussian.py
+++ priors/gaussian.py
@@ -6,13 +6,14 @@ class Gaussian(torch.nn.Module):
     Simple Gaussian multivariate prior without correlation.
     """
 
-    def __init__(self, mu=0, sigma=1):
+    def __init__(self, mu=0, sigma=1, device=None):
         super().__init__()
         self.mu = mu
         self.sigma = sigma
+        self.device = device
 
     def __call__(self, shape):
-        dist = torch.normal(mean=self.mu, std=self.sigma, size=shape)
+        dist = torch.normal(mean=self.mu, std=self.sigma, size=shape, device=self.device)
         return dist
 
     def __str__(self):
