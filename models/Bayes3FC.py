184924e49db0f9fda972e179e9ec49133c01fd84
Khachatur Mirijanyan <khachaturmirijanyan@gmail.com>
2022-04-01 15:24:32 -0700

    FC3

--- models/Bayes3FC.py
+++ models/Bayes3FC.py
@@ -18,3 +18,16 @@ class Bayesian3FC(BaseModule):
 
 
 
+class FC3(BaseModule):
+    """
+    Simple implementation of a 3-layered FC net with ReLU as non-linearity
+    Mainly for easy benchmarking
+    """
+    def __init__(self, features=2, n1=2, n2=2, n3=2, classes=1):
+        super().__init__()
+
+        self.fc1       = nn.Linear(features, n1)
+        self.non_lin1  = nn.PReLU()
+        self.fc2       = nn.Linear(n2, n3)
+        self.non_lin2  = nn.ReLU()
+        self.fc3       = nn.Linear(n3, classes)
