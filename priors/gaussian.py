545f7e3825769a8203dee799530be5a6191c3c14
haakooto <hakonolav.torvik@gmail.com>
2022-04-01 17:49:37 -0700

    redo parameters such they can be learned, hopefully

--- priors/gaussian.py
+++ priors/gaussian.py
@@ -1,20 +1,60 @@
 import torch as torch
+from torch import nn
 
 
-class Gaussian(torch.nn.Module):
+class Gaussian(nn.Module):
     """
     Simple Gaussian multivariate prior without correlation.
     """
 
-    def __init__(self, mu=0, sigma=1, device=None):
+    def __init__(self, in_features, out_features, prior_mu=0, prior_std=1, shape=None, device=None, dtype=None):
         super().__init__()
-        self.mu = mu
-        self.sigma = sigma
         self.device = device
 
-    def __call__(self, shape):
-        dist = torch.normal(mean=self.mu, std=self.sigma, size=shape, device=self.device)
-        return dist
+        self.prior_mu = prior_mu
+        self.prior_sigma = prior_std
+        self.posterior_mu_initial = [prior_mu, prior_std]
+        self.posterior_rho_initial = [prior_mu, prior_std]
+
+        self.W_mu = nn.Parameter(torch.empty((in_features, out_features), device=self.device))
+        self.W_rho = nn.Parameter(torch.empty((in_features, out_features), device=self.device))
+
+        if self.use_bias:
+            self.bias_mu = nn.Parameter(torch.empty((out_features), device=self.device))
+            self.bias_rho = nn.Parameter(torch.empty((out_features), device=self.device))
+        else:
+            self.register_parameter('bias_mu', None)
+            self.register_parameter('bias_rho', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.W_mu.data.normal_(*self.posterior_mu_initial)
+        self.W_rho.data.normal_(*self.posterior_rho_initial)
+
+        if self.use_bias:
+            self.bias_mu.data.normal_(*self.posterior_mu_initial)
+            self.bias_rho.data.normal_(*self.posterior_rho_initial)
+
+    def __call__(self):
+        if not self.frozen:
+            W_eps = torch.empty(self.W_mu.size()).normal_(0, 1).to(self.device)
+            self.W_sigma = torch.log1p(torch.exp(self.W_rho))
+            weight = self.W_mu + W_eps * self.W_sigma
+
+            if self.use_bias:
+                bias_eps = torch.empty(self.bias_mu.size()).normal_(0, 1).to(self.device)
+                self.bias_sigma = torch.log1p(torch.exp(self.bias_rho))
+                bias = self.bias_mu + bias_eps * self.bias_sigma
+            else:
+                bias = None
+        else:
+            weight = self.W_mu
+            bias = self.bias_mu if self.use_bias else None
+
+        return weight, bias
 
     def __str__(self):
         return "A gaussian"
+
+
